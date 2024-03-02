5f251cba9f52d490b65bce092169673d1f9935ab
Adrian Diez Canseco <adrian10cr@proton.me>
2024-03-02 03:22:15 -0400

    side bar finished

--- src/theme.js
+++ src/theme.js
@@ -20,7 +20,7 @@ export const tokens = (mode) => ({
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#434957",
+          400: "#1F2A40", //#1F2A40 #434957
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -162,30 +162,30 @@ export const themeSettings = (mode) => {
           }),
     },
     typography: {
-      fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+      fontFamily: ["Nunito Sans", "sans-serif"].join(","),
       fonstSize: 12,
       h1: {
-        fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+        fontFamily: ["Nunito Sans", "sans-serif"].join(","),
         fonstSize: 40,
       },
       h2: {
-        fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+        fontFamily: ["Nunito Sans", "sans-serif"].join(","),
         fonstSize: 32,
       },
       h3: {
-        fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+        fontFamily: ["Nunito Sans", "sans-serif"].join(","),
         fonstSize: 24,
       },
       h4: {
-        fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+        fontFamily: ["Nunito Sans", "sans-serif"].join(","),
         fonstSize: 20,
       },
       h5: {
-        fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+        fontFamily: ["Nunito Sans", "sans-serif"].join(","),
         fonstSize: 16,
       },
       h6: {
-        fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+        fontFamily: ["Nunito Sans", "sans-serif"].join(","),
         fonstSize: 14,
       },
     },
