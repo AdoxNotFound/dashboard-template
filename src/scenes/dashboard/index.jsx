490c88658ee3b058be1877d8c23586ba50514183
Adrian Diez Canseco <adrian10cr@proton.me>
2024-03-02 18:15:34 -0400

    finished all pages, charts, tables and calendar

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your Dashborad" />
+        <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
       </Box>
     </Box>
   );
